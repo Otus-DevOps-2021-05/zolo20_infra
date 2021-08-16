8c8dc9d0d6805eecb1da0e9354046d7a3d1ba9ab
zolo20 <Java1995>
2021-08-16 22:00:27 +0300

    terraform-2

--- terraform/prod/main.tf
+++ terraform/prod/main.tf
@@ -1,11 +1,12 @@
 provider "yandex" {
+  version                  = "0.35"
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
   zone                     = var.zone
 }
 module "vpc" {
-  source          = "../modules/vpc"
+  source = "../modules/vpc"
 }
 module "app" {
   source           = "../modules/app"
